507bd6eaee7e32d32c5202dac91475e71b413453
Sergey Koval <skoval@exadel.com>
2017-03-31 14:59:16 +0300

    renaming and default design

---
+++ sources/homework3/src/app/github-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'github-search.component.html',
+  styleUrls: ['github-search.component.css']
+})
+export class GithubSearchComponent {
+  title = 'app works!';
+}
--- sources/homework3/src/app/github-search.module.ts
+++ sources/homework3/src/app/github-search.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { GithubSearchComponent } from './github-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    GithubSearchComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GithubSearchComponent]
 })
-export class AppModule { }
+export class GithubSearchModule { }
