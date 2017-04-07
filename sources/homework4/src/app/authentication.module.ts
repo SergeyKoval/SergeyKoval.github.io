234b699d97e1420d84d5bab63133fb78c1b14d2a
Sergey Koval <skoval@exadel.com>
2017-04-07 15:15:43 +0300

    homework 3 renaming

--- sources/homework4/src/app/authentication.module.ts
+++ sources/homework4/src/app/authentication.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AuthenticationComponent]
 })
-export class AppModule { }
+export class AuthenticationModule { }
