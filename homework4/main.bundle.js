15536d47ad124ef4d4cc3596966b48d91611a7a3
Sergey Koval <skoval@exadel.com>
2017-04-09 19:43:29 +0300

    UI commit

--- homework4/main.bundle.js
+++ homework4/main.bundle.js
@@ -691,7 +691,7 @@ RootComponent = __decorate([
 var routes = [
     {
         path: '',
-        redirectTo: 'homework4',
+        redirectTo: 'homework4/authentication',
         pathMatch: 'full'
     },
     {
@@ -729,7 +729,7 @@ var routes = [
     },
     {
         path: '**',
-        redirectTo: 'homework4'
+        redirectTo: 'homework4/authentication'
     }
 ];
 //# sourceMappingURL=routes.js.map
